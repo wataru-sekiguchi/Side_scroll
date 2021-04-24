c1f399de3037ba44b29b9f46f242570785f25caf
wataru-sekiguchi <late.smith.48@gmail.com>
2021-04-24 16:03:13 +0900

    ・アニメーション ・赤ずきんgif ・アニメーションスクリプト ・赤ずきんprefab

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator anim = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if(horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("walk", true);
+        }
+        else if(horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+}
