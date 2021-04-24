eca73075f1df5535a4b4c47774c5bd13791b1dc2
aaafrog <aaafrog@gmail.com>
2021-04-24 19:51:04 +0900

    ボスランダムジャンプ

--- Assets/Scripts/BossManager.cs
+++ Assets/Scripts/BossManager.cs
@@ -9,11 +9,16 @@ public class BossManager : MonoBehaviour
     private float MOVING_SPEED = 100;
 
     private GameObject PlayerObject; // playerオブジェクトを受け取る器
-    public Transform Player; // プレイヤーの座標情報などを受け取る器
+    private Transform Player; // プレイヤーの座標情報などを受け取る器
 
     private float distance;
     private float e_pos;
     private float p_pos;
+
+    private int jumpOn;
+    private float jumpPower = 35000;      // ジャンプの力
+    private bool canJump = false;       // ブロックに設置しているか否か
+    public LayerMask tileMapLayer;		// ブロックレイヤー
 
     // Start is called before the first frame update
     void Start()
@@ -38,22 +43,48 @@ public class BossManager : MonoBehaviour
         p_pos = Player.position.x;  // プレイヤーの世界座標
 
         distance = p_pos - e_pos;
-        //Debug.Log(distance);
 
-        // プレイヤーの方向に動くベクトル(move_speedで速度を調整)
-        //Vector2 force = (p_pos - e_pos) * MOVING_SPEED;
-        if (distance > 50.0f)
-        {
-            //Debug.Log(distance);
-            rbody.velocity = new Vector2(MOVING_SPEED, rbody.velocity.y);
-        }
-        else if(distance < -50.0f)
-        {
-            rbody.velocity = new Vector2(MOVING_SPEED * -1, rbody.velocity.y);
-        }
+
 
         // じわじわ追跡
         //rbody.AddForce(force, ForceMode2D.Force);
+
+
+
+        // 足元の衝突判定、特定のLayerに属するColliberが存在する場合、trueを返す
+        canJump =
+        Physics2D.Linecast(transform.position - (transform.right * 100.5f),
+               transform.position - (transform.up * 100.3f), tileMapLayer) ||
+        Physics2D.Linecast(transform.position + (transform.right * 100.5f),
+               transform.position - (transform.up * 100.3f), tileMapLayer);
+
+        jumpOn = Random.Range(0, 1000);
+
+
+        if (canJump)
+        {
+            if (jumpOn == 0)
+            {
+                //Debug.Log("jump");
+                rbody.AddForce(Vector2.up * jumpPower);
+            }
+
+            // プレイヤーの方向に動く(move_speedで速度を調整)
+            if (distance > 50.0f)
+            {
+                //Debug.Log(distance);
+                rbody.velocity = new Vector2(MOVING_SPEED, rbody.velocity.y);
+            }
+            else if (distance < -50.0f)
+            {
+                rbody.velocity = new Vector2(MOVING_SPEED * -1, rbody.velocity.y);
+            }
+
+
+        }
+
+
+
     }
 
 
@@ -63,6 +94,7 @@ public class BossManager : MonoBehaviour
         //rbody.velocity = new Vector2(MOVING_SPEED, rbody.velocity.y);   // Rigidbody2Dコンポーネントのvelocityに速度を設定、y軸は変更がないのでそのまま
 
     }
+
 
 
 }
